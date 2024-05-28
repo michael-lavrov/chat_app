9f3ea3bf1fc75eac181eeb5d4c7f5abed4a70ba1
lavrov14798 <michael.lavrov@mail.huji.ac.il>
2024-05-28 19:19:22 +0300

    Adding functionality to server.py and client.py

--- client.py
+++ client.py
@@ -1,8 +1,59 @@
 import socket
 import threading
 
+EMPTY_STR = ''
+MSG_MAX_LEN = 2048
+CODING_STANDARD = 'utf-8'
+
 HOST = '127.0.0.1'
 PORT = 1234
+
+
+def listen_for_messages_from_server(client):
+
+    while True:
+        message = client.recv(MSG_MAX_LEN).decode(CODING_STANDARD)
+        if message != EMPTY_STR:
+            split_msg = message.split('~')
+            username = split_msg[0]
+            content = EMPTY_STR
+            for i in range(1, len(split_msg)):
+                content += split_msg[i]
+
+            print(f"[{username}] {content}")
+
+        else:
+            print("Message is empty")
+
+
+def send_msg_to_server(client):
+
+    while True:
+
+        message = input("Message: ")
+        if message != EMPTY_STR:
+            client.sendall(message.encode())
+        else:
+            print("Message is empty")
+            exit(0)
+
+
+def communicate_to_server(client):
+
+    #TODO: Add an option to retry entering the username
+
+    # Sending the username to the server
+    username = input("Choose a username: ")
+    #TODO: Username cannot containt '~'
+    if username != EMPTY_STR:
+        client.sendall(username.encode())
+    else:
+        print("Username cannot be empty")
+        exit(0)
+
+    threading.Thread(target=listen_for_messages_from_server, args=(client, )).start()
+    send_msg_to_server(client)
+
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -11,6 +62,9 @@ def main():
         print("Successfully connected to server")
     except:
         print(f"Unable to connect to server {HOST}, at port {PORT}")
+
+    communicate_to_server(client)
+
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,12 +1,84 @@
 import socket
 import threading
 
+EMPTY_STR = ''
+MSG_MAX_LEN = 2048
+CODING_STANDARD = 'utf-8'
+
 HOST = '127.0.0.1'
 PORT = 1234
 LISTENERS_LIMIT = 5
 
+active_users = {}
+
+
+def listen_for_messages(username):
+    """
+    Receives messages from the client and echoes them to all users
+    :param username: Username of a client
+    :return: None
+    """
+    while True:
+
+        message = active_users[username].recv(MSG_MAX_LEN).decode(CODING_STANDARD)
+        if message != EMPTY_STR:
+            final_msg = username + '~' + message
+            send_msg_to_all(final_msg)
+        else:
+            print(f"The message sent from client {username} is empty")
+
+
+def send_msg_to_all(message):
+    """
+    Sends a given message to all active users
+    :param message: The message to be sent
+    :return: None
+    """
+    for user in active_users:
+        send_msg_to_single_client(active_users[user], message)
+
+
+def send_msg_to_single_client(client, message):
+    """
+    Sends message to a single client
+    :param client: The client socket object
+    :param message: The message to be sent
+    :return: None
+    """
+    client.sendall(message.encode())
+
+
+def client_handler(client):
+    """
+    Handles the client's requests, runs on a separate thread.
+    :param client: The client socket object
+    :return: None
+    """
+    while True:
+
+        # Receiving username from the client
+        username = client.recv(MSG_MAX_LEN).decode(CODING_STANDARD)
+
+        if username != EMPTY_STR:
+
+            if username not in active_users:
+                # Adding username to active users dict
+                active_users[username] = client
+                prompt_msg = "SERVER" + '~' + f"{username} has joined the chat"
+                send_msg_to_all(prompt_msg)
+                listen_for_messages(username)
+                break
+            else:
+                print(f"{username} is already taken")
+        else:
+            print("Username is an empty string")
+
+    # Creating a thread to listen to user messages
+    threading.Thread(target=listen_for_messages, args=(username, )).start()
+
 
 def main():
+
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         server.bind((HOST, PORT))
@@ -19,6 +91,7 @@ def main():
     while True:
         client, address = server.accept()
         print(f"Successfully connected to client {address[0]} {address[1]}")
+        threading.Thread(target=client_handler, args=(client, )).start()
 
 
 if __name__ == "__main__":
